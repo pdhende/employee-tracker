161a7d25413797e8c0ee8519e17f0db1ab27ac57
pdhende <pdhendework@gmail.com>
2022-03-22 00:19:48 -0400

    Created schema.sql and created database and department table

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
