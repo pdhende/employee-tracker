d1c088e24adcccd68baecd99ef77eb731be9fa86
pdhende <pdhendework@gmail.com>
2022-03-22 01:08:52 -0400

    Created employee table

--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,17 @@ CREATE TABLE roles (
     REFERENCES department(id)
     ON DELETE SET NULL
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT DEFAULT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
