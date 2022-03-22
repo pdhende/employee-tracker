e8616882c52b232dd4df049eb081a033d1c0c811
pdhende <pdhendework@gmail.com>
2022-03-22 00:35:55 -0400

    Created roles table

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,13 @@ CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
