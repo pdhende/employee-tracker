96471ebd0aeb5ffc0581c1ae29e080d0e0a89239
pdhende <pdhendework@gmail.com>
2022-03-23 13:56:40 -0400

    Added code for promise based queries

--- getData.js
+++ getData.js
@@ -1,10 +1,17 @@
 const dbConnect = require('./config/connection');
 
-let Viewalldepartments = 'SELECT * FROM departments';
+const promiseConn = dbConnect.promise();
+let sql = 'SELECT * FROM department';
 
 function sendQuery(optionVal) {
     console.log(`in sendquery ${optionVal}`);
-    
+    optionVal = optionVal.replace(/\s/g, '');
+    console.log(optionVal);
+    promiseConn.query(sql)
+        .then((rows) => {
+            console.log(rows);
+        });
 }
+
 
 module.exports = sendQuery;
