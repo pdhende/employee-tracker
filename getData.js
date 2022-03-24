c50f5e8d6a2c3b9c85ce8c2732f9b17d8f9b2e34
pdhende <pdhendework@gmail.com>
2022-03-24 01:00:44 -0400

    Display all departments and roles

--- getData.js
+++ getData.js
@@ -1,20 +1,86 @@
 const dbConnect = require('./config/connection');
-const conTable = require('console.table')
+const conTable = require('console.table');
 
 const promiseConn = dbConnect.promise();
-let sql = 'SELECT * FROM department';
-
-function sendQuery(optionVal) {
-    // console.log(`in sendquery ${optionVal}`);
-    optionVal = optionVal.replace(/\s/g, '');
-    // console.log(optionVal);
-    promiseConn.query(sql)
-        .then((data) => {
-            const deptTbl = conTable.getTable(data[0]);
-            console.log("\n"+deptTbl);
-        })
-        .catch(console.log);
-};
 
 
-module.exports = sendQuery;
+const viewDept = new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM department";
+        dbConnect.query(sql, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            // console.log(results);
+                        // const deptTbl = conTable.getTable(results);
+                        // console.log("\n\n" + deptTbl);
+            // console.log("HI");
+            // console.log(results);
+            resolve(results);
+        });
+    });
+
+const viewRoles = new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM roles";
+    dbConnect.query(sql, (err, results) => {
+        if (err) {
+            return reject(err);
+        }
+        // console.log(results);
+                    // const deptTbl = conTable.getTable(results);
+                    // console.log("\n\n" + deptTbl);
+        // console.log("HI");
+        // console.log(results);
+        resolve(results);
+    });
+});
+
+
+// const sendQuery = (optionVal) => {
+//     return new Promise((resolve, reject) => {
+//         promiseConn.query(sql, (err, results) => {
+//             if (err) {
+//                 return reject(err);
+//             }
+//             resolve(results);
+//         });
+//         //     .then((data) => {
+//         //         const deptTbl = conTable.getTable(data[0]);
+//         //         console.log("\n" + deptTbl);
+//         //     })
+//         //     .catch(err => {
+//         //         console.log
+//         //     });
+//         // // const functionName = optionVal.replace(/\s/g, '');
+//         // if (err) {
+//         //     return reject(err);
+//         // }
+//         // resolve(data);
+//     });
+// }
+// let sql = 'SELECT * FROM department';
+
+// function sendQuery(optionVal) {
+//     // console.log(`in sendquery ${optionVal}`);
+//     optionVal = optionVal.replace(/\s/g, '');
+//     // console.log(optionVal);
+//     promiseConn.query(sql)
+//         .then((data) => {
+//             const deptTbl = conTable.getTable(data[0]);
+//             console.log("\n"+deptTbl);
+//         })
+//         .catch(err => {
+//             console.log
+//         });
+
+// };
+
+// fs.readFile(fileName, encoding, (err, data) => {
+//     if (err) {
+//         return reject(err);
+//     }
+
+//     resolve(data);
+// });
+
+
+module.exports = { viewDept, viewRoles};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const inquirer = require('inquirer');
-const getData = require('./getData');
+const { viewDept, viewRoles } = require('./getData');
+const conTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,27 +17,62 @@ const optionArr = [
         name: "optionVal",
         message: `What would you like to do?`,
         choices: [
-                    'View all departments',
-                    'View all roles',
-                    'View all employees',
-                    'Add a department',
-                    'Add a role',
-                    'Add an employee',
-                    'Update an employee role'
-                ],
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role'
+        ],
     }
 ];
 
 // Function to present options to the user
-function init() {
-    inquirer.prompt(optionArr).then((answers) => {
-        // console.log(answers);
-        // console.log(sq);
-        getData(answers.optionVal);
-        // console.log("Query done!");
-    });
+function showOptions() {
+    inquirer.prompt(optionArr)
+        .then((answers) => {
+            switch (answers.optionVal) {
+                case 'View all departments':
+                  viewDept.then((results) => {
+                    const deptTbl = conTable.getTable(results);
+                    console.log("\n\n" + deptTbl);
+                    //   console.log("In then");
+                    showOptions();
+                    });
+                  break;
+                case 'View all roles':
+                    viewRoles.then((results) => {
+                        const deptTbl = conTable.getTable(results);
+                        console.log("\n\n" + deptTbl);
+                        //   console.log("In then");
+                            showOptions();
+                        });
+                    break;
+                case 'View all employees':
+                    viewEmployee();
+                    break;
+                default:
+                  console.log(`Sorry invalid choice`);
+              };
+            // console.log(answers);
+            // console.log(sq);
+            // getData(answers.optionVal)
+            // .then((results) => {
+            //     const deptTbl = conTable.getTable(results);
+            //     console.log("\n" + deptTbl);
+            //     showOptions();
+            // });
+            // console.log("Query done!");
+        })
+        // .then((results) => {
+        //     console.log(results);
+        //         const deptTbl = conTable.getTable(results);
+        //         console.log("\n" + deptTbl);
+        //         showOptions();
+        //     });
 };
 
 // Call function
-init();
+showOptions();
 
