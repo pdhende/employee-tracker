5407848ba95e9d5566be2e3b39d847fa9a1fd3cd
pdhende <pdhendework@gmail.com>
2022-03-24 01:37:51 -0400

    Display additional questions for adding dept

--- getData.js
+++ getData.js
@@ -3,18 +3,12 @@ const conTable = require('console.table');
 
 const promiseConn = dbConnect.promise();
 
-
 const viewDept = new Promise((resolve, reject) => {
         const sql = "SELECT * FROM department";
         dbConnect.query(sql, (err, results) => {
             if (err) {
                 return reject(err);
             }
-            // console.log(results);
-                        // const deptTbl = conTable.getTable(results);
-                        // console.log("\n\n" + deptTbl);
-            // console.log("HI");
-            // console.log(results);
             resolve(results);
         });
     });
@@ -25,62 +19,8 @@ const viewRoles = new Promise((resolve, reject) => {
         if (err) {
             return reject(err);
         }
-        // console.log(results);
-                    // const deptTbl = conTable.getTable(results);
-                    // console.log("\n\n" + deptTbl);
-        // console.log("HI");
-        // console.log(results);
         resolve(results);
     });
 });
 
-
-// const sendQuery = (optionVal) => {
-//     return new Promise((resolve, reject) => {
-//         promiseConn.query(sql, (err, results) => {
-//             if (err) {
-//                 return reject(err);
-//             }
-//             resolve(results);
-//         });
-//         //     .then((data) => {
-//         //         const deptTbl = conTable.getTable(data[0]);
-//         //         console.log("\n" + deptTbl);
-//         //     })
-//         //     .catch(err => {
-//         //         console.log
-//         //     });
-//         // // const functionName = optionVal.replace(/\s/g, '');
-//         // if (err) {
-//         //     return reject(err);
-//         // }
-//         // resolve(data);
-//     });
-// }
-// let sql = 'SELECT * FROM department';
-
-// function sendQuery(optionVal) {
-//     // console.log(`in sendquery ${optionVal}`);
-//     optionVal = optionVal.replace(/\s/g, '');
-//     // console.log(optionVal);
-//     promiseConn.query(sql)
-//         .then((data) => {
-//             const deptTbl = conTable.getTable(data[0]);
-//             console.log("\n"+deptTbl);
-//         })
-//         .catch(err => {
-//             console.log
-//         });
-
-// };
-
-// fs.readFile(fileName, encoding, (err, data) => {
-//     if (err) {
-//         return reject(err);
-//     }
-
-//     resolve(data);
-// });
-
-
-module.exports = { viewDept, viewRoles};
+module.exports = { viewDept, viewRoles };
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// // Array of options for user to view/update company database
+// Array of options for user to view/update company database
 const optionArr = [
     {
         type: "list",
@@ -28,51 +28,52 @@ const optionArr = [
     }
 ];
 
+// Questions for adding new department
+const deptQues = [
+    {
+        type: "input",
+        name: "deptName",
+        message: `What is the name of the department?`,
+    }
+];
+
 // Function to present options to the user
-function showOptions() {
-    inquirer.prompt(optionArr)
+function showOptions(quesArr) {
+    inquirer.prompt(quesArr)
         .then((answers) => {
-            switch (answers.optionVal) {
-                case 'View all departments':
-                  viewDept.then((results) => {
-                    const deptTbl = conTable.getTable(results);
-                    console.log("\n\n" + deptTbl);
-                    //   console.log("In then");
-                    showOptions();
-                    });
-                  break;
-                case 'View all roles':
-                    viewRoles.then((results) => {
-                        const deptTbl = conTable.getTable(results);
-                        console.log("\n\n" + deptTbl);
-                        //   console.log("In then");
-                            showOptions();
-                        });
-                    break;
-                case 'View all employees':
-                    viewEmployee();
-                    break;
-                default:
-                  console.log(`Sorry invalid choice`);
-              };
-            // console.log(answers);
-            // console.log(sq);
-            // getData(answers.optionVal)
-            // .then((results) => {
-            //     const deptTbl = conTable.getTable(results);
-            //     console.log("\n" + deptTbl);
-            //     showOptions();
-            // });
-            // console.log("Query done!");
-        })
-        // .then((results) => {
-        //     console.log(results);
-        //         const deptTbl = conTable.getTable(results);
-        //         console.log("\n" + deptTbl);
-        //         showOptions();
-        //     });
+            console.log(answers.optionVal);
+            sendQuery(answers.optionVal);
+        });
 };
 
-// Call function
-showOptions();
+// Function to invoke functions based on the user selection
+function sendQuery(val) {
+    switch (val) {
+            case 'View all departments':
+                viewDept.then((results) => {
+                    const deptTbl = conTable.getTable(results);
+                    console.log("\n\n" + deptTbl);
+                    showOptions(optionArr);
+                });
+                break;
+            case 'View all roles':
+                viewRoles.then((results) => {
+                    const deptTbl = conTable.getTable(results);
+                    console.log("\n\n" + deptTbl);
+                    showOptions(optionArr);
+                });
+                break;
+            case 'View all employees':
+                viewEmployee();
+                break;
+            case 'Add a department':
+                showOptions(deptQues);
+                break;
+            default:
+                console.log(`Sorry invalid choice`);
+        };
+}
+
+// First call to the function
+showOptions(optionArr);
 
